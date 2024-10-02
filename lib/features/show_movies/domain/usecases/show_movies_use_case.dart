6989d31b0af76abbea0b9a19ffec6f685a1457d6
MohamedMohamedSaleh <alrisha77777@gmail.com>
2024-10-02 23:47:07 +0300

    add usecase class

--- lib/features/show_movies/domain/usecases/show_movies_use_case.dart
+++ lib/features/show_movies/domain/usecases/show_movies_use_case.dart
@@ -1,17 +1,23 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../../../../core/use_cases/use_case.dart';
 import '../entities/movies_entity.dart';
 import '../repository/movies_repository.dart';
 /// this is the use case that i will use in the app. 
 /// in this case i will get the movies. 
 /// i will use the get movies method from the movies repo. 
-class ShowMoviesUseCase {
+class ShowMoviesUseCase extends UseCase<List<MoviesEntity>, NoParams> {
   final MoviesRepo moviesRepo;
 
   ShowMoviesUseCase({required this.moviesRepo});
   
-  Future<Either<Failure, List<MoviesEntity>>> call() {
-    return moviesRepo.getMovies();
+  @override
+  Future<Either<Failure, List<MoviesEntity>>> call([NoParams? params]) {
+   
+    throw UnimplementedError();
   }
+  
 }
+
+
